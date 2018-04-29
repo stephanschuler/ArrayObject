7a99705aee0fd087df9b1bc8d9c75868b64cdddb
Stephan Schuler <stephan.schuler@netlogix.de>
2018-04-29 13:44:36 +0200

    Introduce GroupOperator

---
+++ definition/Operator/GroupOperator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace StephanSchuler\ArrayObject;
+
+/**
+ * @method ArrayObject groupSubsequent(callable $compare, int $groupSize)
+ * @see \StephanSchuler\ArrayObject\Operator\GroupOperator::groupSubsequent()
+ *
+ * @method ArrayObject groupBatch(callable $compare, int $groupSize, int $numberOfGroups = PHP_INT_MAX)
+ * @see \StephanSchuler\ArrayObject\Operator\GroupOperator::groupBatch()
+ */
+class ArrayObject
+{
+}
---
+++ src/Operator/GroupOperator.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace StephanSchuler\ArrayObject\Operator;
+
+use Generator;
+use Traversable;
+
+class GroupOperator extends AbstractOperator
+{
+    public static function groupSubsequent(Traversable $data, callable $compare, int $groupSize): Generator
+    {
+        $lastGroupDiscriminator = null;
+        $group = [];
+        foreach ($data as $key => $value) {
+            $discriminator = $compare($value);
+            if ($discriminator !== $lastGroupDiscriminator) {
+                if (count($group)) {
+                    yield $lastGroupDiscriminator => $group;
+                }
+                $group = [];
+            }
+            if (count($group) <= $groupSize) {
+                $group[$key] = $value;
+            }
+            $lastGroupDiscriminator = $discriminator;
+        }
+        if (count($group)) {
+            yield $lastGroupDiscriminator => $group;
+        }
+    }
+
+    public static function groupBatch(
+        Traversable $data,
+        callable $compare,
+        int $groupSize,
+        int $numberOfGroups = PHP_INT_MAX
+    ): Generator {
+        $groups = [];
+        foreach ($data as $key => $value) {
+            $discriminator = $compare($value);
+            if (!isset($groups[$discriminator])) {
+                if (count($groups) >= $numberOfGroups) {
+                    break;
+                }
+                $groups[$discriminator] = [];
+            }
+            if (count($groups[$discriminator]) < $groupSize) {
+                $groups[$discriminator][$key] = $value;
+            }
+        }
+        yield from $groups;
+    }
+}
